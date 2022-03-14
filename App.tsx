9d0e7847673ae17197522fd6eebafb4577c0b10b
Александр <kozlovetsalexandr@gmail.com>
2022-03-14 13:11:30 +0200

    Wrap an app by store and navigation providers, add wrapper style

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider as StoreProvider } from 'react-redux';
+import { store } from './src/store';
+import AppNavigator from './src/navigation';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaView></SafeAreaView>;
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.wrapper}>
+          <AppNavigator />
+        </SafeAreaView>
+      </NavigationContainer>
+    </StoreProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+});
 
 export default App;
