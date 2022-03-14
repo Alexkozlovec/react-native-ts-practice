42ef5d62f19a56154bbaa6bec87930e36df1e15a
Александр <kozlovetsalexandr@gmail.com>
2022-03-14 09:34:40 +0200

    Create types for received fata

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,76 @@
 export type RootStackParamList = {
   Gallery: undefined;
 };
+
+// unsplash data
+interface IUnsplashImageUrls {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+}
+
+interface IUnsplashImageLinks {
+  self: string;
+  html: string;
+  download: string;
+  download_location: string;
+}
+
+interface IUnsplashUserLinks {
+  self: string;
+  html: string;
+  photos: string;
+  likes: string;
+  portfolio: string;
+}
+
+interface IUnsplashProfileImage {
+  small: string;
+  medium: string;
+  large: string;
+}
+
+interface IUnsplashUser {
+  id: string;
+  username: string;
+  name: string;
+  portfolio_url: string;
+  bio: string;
+  location: string;
+  total_likes: number;
+  total_photos: number;
+  total_collections: number;
+  instagram_username: string | null;
+  twitter_username: string | null;
+  profile_image: IUnsplashProfileImage;
+  links: IUnsplashUserLinks;
+}
+
+interface IUnsplashCollection {
+  id: number;
+  title: string;
+  published_at: string;
+  last_collected_at: string;
+  updated_at: string;
+  cover_photo: string | null;
+  user: null;
+}
+
+export interface IUnsplashImage {
+  id: string;
+  created_at: string;
+  updated_at: string | null;
+  width: number;
+  height: number;
+  color: string;
+  blur_hash: string;
+  likes: number;
+  liked_by_user: boolean;
+  description: string | null;
+  user: IUnsplashUser;
+  current_user_collections: IUnsplashCollection[];
+  urls: IUnsplashImageUrls;
+  links: IUnsplashImageLinks;
+}
